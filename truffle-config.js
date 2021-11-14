d514f6af9f8dc0782ea06a9c69b86d02637652c9
Ben Leimberger <wbleimberger@gmail.com>
2021-11-14 15:25:38 -0500

    Init commit

---
+++ truffle-config.js
@@ -0,0 +1,14 @@
+const path = require("path");
+
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  contracts_build_directory: path.join(__dirname, "app/src/contracts"),
+  networks: {
+    develop: { // default with truffle unbox is 7545, but we can use develop to test changes, ex. truffle migrate --network develop
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*"
+    }
+  }
+};
