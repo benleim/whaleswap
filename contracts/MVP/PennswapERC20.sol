747ddc3c38c0dbef2e42c1d15d3b8cfb50459029
Ben Leimberger <wbleimberger@gmail.com>
2022-01-15 19:19:51 -0500

    uniV2 core contracts

--- contracts/MVP/PennswapERC20.sol
+++ contracts/MVP/PennswapERC20.sol
@@ -79,7 +79,7 @@ contract PennswapERC20 is IPennswapERC20 {
     }
 
     function permit(address owner, address spender, uint value, uint deadline, uint8 v, bytes32 r, bytes32 s) external {
-        require(deadline >= block.timestamp, 'UniswapV2: EXPIRED');
+        require(deadline >= block.timestamp, 'Pennswap: EXPIRED');
         bytes32 digest = keccak256(
             abi.encodePacked(
                 '\x19\x01',
@@ -88,7 +88,7 @@ contract PennswapERC20 is IPennswapERC20 {
             )
         );
         address recoveredAddress = ecrecover(digest, v, r, s);
-        require(recoveredAddress != address(0) && recoveredAddress == owner, 'UniswapV2: INVALID_SIGNATURE');
+        require(recoveredAddress != address(0) && recoveredAddress == owner, 'Pennswap: INVALID_SIGNATURE');
         _approve(owner, spender, value);
     }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,17 @@
 const SimpleStorage = artifacts.require("SimpleStorage");
-const TutorialToken = artifacts.require("TutorialToken");
 const ComplexStorage = artifacts.require("ComplexStorage");
 const SimpleToken1 = artifacts.require("SimpleToken1");
 const SimpleToken2 = artifacts.require("SimpleToken2");
+const Factory = artifacts.require("Factory");
 
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
-  deployer.deploy(TutorialToken);
   deployer.deploy(ComplexStorage);
+
+  // Test Tokens
   deployer.deploy(SimpleToken1);
   deployer.deploy(SimpleToken2);
+
+  // AMM contracts
+  deployer.deploy(Factory, "0x0000000000000000000000000000000000000000")
 };
